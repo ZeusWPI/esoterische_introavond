264634dd2a08485fe565591a1e425a525cd35d7f
Timo De Waele <timo.dewaele@ugent.be>
2023-10-11 22:58:20 +0200

    fucking asm

--- 2023/1/timpy.asm
+++ 2023/1/timpy.asm
@@ -1,10 +1,11 @@
 section .data
 align 8
-message: db "Zeus is de max!", 0Ah, 00h   ; String, followed by newline and nullbyte
+message: db "Zeus is de max!", 00h   ; String, followed by newline and nullbyte
 
 section .text
 global _main
 extern _strlen               ; strlen from string.h
+extern _puts
 
 _main:
   ; Calculate length of message
@@ -12,11 +13,8 @@ _main:
   call _strlen
   mov r8, rax
 
-  mov rax, 0x02000004       ; Write syscall
-  mov rdi, 1                ; Write to stdout
-  lea rsi, [rel message]    ; Address of message
-  mov rdx, r8               ; Length of string
-  syscall
+  lea rdi, [rel message]
+  call _puts
 
   mov rax, 0x02000001       ; Exit syscall
   xor rdi, rdi              ; Exit code 0
